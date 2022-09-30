e14313557e3513cd88a4ee24b0eb0c02a248af0f
Jalil SA <61639983+jxlil@users.noreply.github.com>
2022-09-30 16:21:26 -0500

    feat: add status scraper

--- api/v1/scrape/router.py
+++ api/v1/scrape/router.py
@@ -6,8 +6,13 @@ from fastapi import APIRouter
 
 from .models import ResponseBooks
 
-router = APIRouter(prefix="/scrape", tags=["Scrape"])
+router = APIRouter(prefix="/v1/scrape", tags=["Scrape"])
 processor = Processor(settings=get_project_settings())
+
+
+@router.get("/")
+async def check():
+    return {"status": "running"}
 
 
 @router.get("/books", response_model=ResponseBooks)
